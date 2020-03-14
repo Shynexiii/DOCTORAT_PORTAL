782ae1e63e1747371cdb5fed6192a72d0d340ada
Shynexiii <habibaminou2@gmail.com>
2020-03-14 20:01:16 +0100

    Update Master blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -158,7 +158,7 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      The TEAM
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2019-2020 <a href="{{ route('home')}}">Doctorat.test</a>.</strong> All rights reserved.
